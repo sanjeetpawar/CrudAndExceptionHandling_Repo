72ad56398d267751c1995a3dd1f00bdfcbef6324
Aniket <Aniket@DESKTOP-GRCU7KB>
2022-10-04 16:17:11 +0530

    packing war chnages added in pom and main class

--- src/main/java/com/sanjeet/demo/CurdandExceptionHandlingApplication.java
+++ src/main/java/com/sanjeet/demo/CurdandExceptionHandlingApplication.java
@@ -2,9 +2,10 @@ package com.sanjeet.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CurdandExceptionHandlingApplication {
+public class CurdandExceptionHandlingApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurdandExceptionHandlingApplication.class, args);
